e529b0f10aeb1f81da39ceff664640e26567d89f
jsngyoon <jsngyoon@gmail.com>
2018-11-21 20:12:58 +0900

    UserDao.java findAll() 구현

--- src/test/java/next/WebServerLauncher.java
+++ src/test/java/next/WebServerLauncher.java
@@ -2,6 +2,7 @@ package next;
 
 import java.io.File;
 
+import org.apache.catalina.connector.Connector;
 import org.apache.catalina.startup.Tomcat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,7 +14,8 @@ public class WebServerLauncher {
         String webappDirLocation = "webapp/";
         Tomcat tomcat = new Tomcat();
         tomcat.setPort(8080);
-
+        Connector connector = tomcat.getConnector();
+		connector.setURIEncoding("UTF-8");
         tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
         logger.info("configuring app with basedir: {}", new File("./" + webappDirLocation).getAbsolutePath());
 
